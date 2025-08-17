1b71939d6156c1cee23fab2419677b8a78f94770
megha03-coder <meghachauhan310@gmail.com>
2025-08-17 11:48:36 +0530

    create a navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <nav className="bg-white shadow-lg fixed top-0 left-0 right-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-center items-center h-16">
-          <div className="text-center">
-            <a href="#" className="text-3xl font-bold text-indigo-600">
-              Portfolio
-            </a>
-          </div>
-        </div>
+    <nav className="bg-gray-800 p-4">
+      <h1 className="text-white text-center text-2xl">Website Title</h1>
+      <hr className="my-2 border-gray-600" />
+      <div className="text-white text-right mt-2">
+        <a href="#link1" className="mx-2" >Link 1</a> 
+        <a href="#link2" className="mx-2">Link 2</a> 
+        <a href="#link3" className="mx-2">Link 3</a>  
+        <a href="#link4" className="mx-2">Link 4</a>
       </div>
+      
     </nav>
   );
-}
+};
 
 export default Navbar;
