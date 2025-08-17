d266ad56deaf3424e53252b2f52d13727af58f40
megha03-coder <meghachauhan310@gmail.com>
2025-08-17 11:16:19 +0530

    install dependencies and modify files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/navbar'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import MainLayout from './components/MainLayout';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <Navbar  />
-        
+      <MainLayout />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
