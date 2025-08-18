d6bbc325caf620db65e4ac6104b491de2703fc5a
megha03-coder <meghachauhan310@gmail.com>
2025-08-18 12:30:01 +0530

    add another components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import MainLayout from './components/MainLayout';
+import React from 'react';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+import TeamSection from './components/TeamSection';
+import CTASection from './components/CTASection';
+import Footer from './components/Footer';
 
-function App() {
-  const [currentPage, setCurrentPage] = useState('home');
-
+const App = () => {
   return (
-    <>
-      <MainLayout currentPage={currentPage} setCurrentPage={setCurrentPage} />
-    </>
+    <div className="min-h-screen bg-white">
+      <Header />
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <TeamSection />
+        <CTASection />
+      </main>
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
